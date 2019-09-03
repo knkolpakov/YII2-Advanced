5411cb837bae028ae331885903693a11d0338dce
Konstantin <kn.kolpakov@gmail.com>
2019-09-03 22:53:45 +0300

    Lesson-1

---
+++ frontend/views/hello/hello.php
@@ -0,0 +1,7 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Hello world';
+?>
+<h2>Hello World</h2>
---
+++ frontend/views/site/task-tracker.php
@@ -0,0 +1,8 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Task tracker';
+?>
+<h2>Task tracker</h2>
+<div>Разместить статус по задачам пример доски как в Jira ( Похожа на SRM:) )</div>
