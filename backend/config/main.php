b19706c077c150ff2fe79394db9ce8fe08e87276
Konstantin <kn.kolpakov@gmail.com>
2019-09-10 23:14:42 +0300

    Lesson 3

--- backend/config/main.php
+++ backend/config/main.php
@@ -42,6 +42,8 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
             ],
         ],
         */
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -41,6 +41,8 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
             ],
         ],
         
